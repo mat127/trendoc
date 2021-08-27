0c39ca65c45a2a16f0106ee99156177a759c9cc8
mat127 <>
2021-08-27 19:03:12 +0200

    + Spring initializr

---
+++ src/main/java/com/github/mat127/trendoc/TrendocApplication.java
@@ -0,0 +1,13 @@
+package com.github.mat127.trendoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mat127/trendoc/TrendocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mat127.trendoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
