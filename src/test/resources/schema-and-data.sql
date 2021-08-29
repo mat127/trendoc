1ffa35f8344336844f5b2938f0bce6d8a2494138
mat127 <>
2021-08-29 10:13:54 +0200

    # db schema fixed

--- src/test/resources/schema-and-data.sql
+++ src/test/resources/schema-and-data.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS `document_stats`;
 CREATE TABLE `document_stats` (
   `document_id` varchar(36) NOT NULL,
   `day` date NOT NULL,
-  `display_count` int DEFAULT '0',
-  `display_trend` int DEFAULT '0',
+  `display_count` int NOT NULL DEFAULT '0',
+  `display_trend` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`day`,`document_id`)
 );
 
